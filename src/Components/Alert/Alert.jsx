4800612c9c07c2a869774554f0e4303b1a844abf
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2020-06-12 04:45:35 -0300

    Alert Criado

---
+++ src/Components/Alert/Alert.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Alert.css'
+
+export default props => {
+     return (
+          <div className="alert-container">
+              <h1>👆 Need Geolocation</h1>
+          </div>
+     )
+}
